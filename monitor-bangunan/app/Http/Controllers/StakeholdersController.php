7739535478dc051c13787b685a0561efa771b632
alfito24 <khansaalfito@gmail.com>
2022-04-08 22:42:16 +0700

    Update StakeholdersController.php

--- monitor-bangunan/app/Http/Controllers/StakeholdersController.php
+++ monitor-bangunan/app/Http/Controllers/StakeholdersController.php
@@ -11,26 +11,6 @@ class StakeholdersController extends Controller
         return view('registerStakeholders');
     }
     public function store(Request $request){
-        // $validatedData=  $request->validate([
-        //     'name' => 'required|max:25',
-        //     'peran' => 'required',
-        //     'kategori' => 'required',
-        //     'pengetahuan' => 'required',
-        //     'usia' => 'required',
-        //     'pendidikan' => 'required',
-        // ]);
-        // if($validatedData['kategori'] == 'industri'){
-        //     $validatedData['simbol'] = 'I';
-        // }
-        // if($validatedData['kategori'] == 'pemilik'){
-        //     $validatedData['simbol'] = 'P';
-        // }
-        // if($validatedData['kategori'] == 'pengguna'){
-        //     $validatedData['simbol'] = 'PB';
-        // }
-        // if($validatedData['kategori'] == 'masyarakat'){
-        //     $validatedData['simbol'] = 'M';
-        // }
         Stakeholders::create([
             'nama'=>$request->name,
             'peran'=>$request->peran,
