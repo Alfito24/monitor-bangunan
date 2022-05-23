a6051db0c0881729e029abe40bf580e887932152
lsatriosim <liefran123@gmail.com>
2022-05-23 21:26:06 +0700

    migration survey

--- monitor-bangunan/database/migrations/2022_05_23_142334_create_proyek_survey.php
+++ monitor-bangunan/database/migrations/2022_05_23_142334_create_proyek_survey.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProyekUserSurveyTable extends Migration
+class CreateProyekSurvey extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,12 @@ class CreateProyekUserSurveyTable extends Migration
      */
     public function up()
     {
-        Schema::create('proyek_user_survey', function (Blueprint $table) {
+        Schema::create('proyek_survey', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('proyek_id')->unsigned();
-            $table->bigInteger('user_id')->unsigned();
             $table->bigInteger('survey_id')->unsigned();
             $table->foreign('proyek_id')->references('id')->on('proyeks')->onDelete('cascade');
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('survey_id')->references('id')->on('surveys')->onDelete('cascade');
-            $table->integer('status');
             $table->timestamps();
         });
     }
@@ -33,6 +30,6 @@ class CreateProyekUserSurveyTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('proyek_user_survey');
+        Schema::dropIfExists('proyek_survey');
     }
 }
---
+++ monitor-bangunan/database/migrations/2022_05_23_142456_create_survey_user_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSurveyUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('survey_user', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('survey_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('survey_id')->references('id')->on('surveys')->onDelete('cascade');
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('survey_user');
+    }
+}
