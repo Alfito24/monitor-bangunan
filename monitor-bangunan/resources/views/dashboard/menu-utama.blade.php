ab67100c85482b245fd972a116c11421c10335ee
lsatriosim <liefran123@gmail.com>
2022-06-13 23:34:00 +0700

    halaman form survey

--- monitor-bangunan/resources/views/dashboard/menu-utama.blade.php
+++ monitor-bangunan/resources/views/dashboard/menu-utama.blade.php
@@ -178,11 +178,9 @@
                                                                 <th>Status</th>
                                                             </tr>
                                                         </thead>
-
                                                         @foreach ($survey as $sv)
                                                             @foreach ($responden as $res)
                                                                 @if ($res->survey_id === $sv->survey_id)
-                                                                    @if ($sv->id !== Auth::id())
                                                                         <tbody>
                                                                             <tr>
                                                                                 <td>{{ $loop->iteration }}</td>
@@ -210,7 +208,6 @@
                                                                                 @endif
                                                                             </tr>
                                                                         </tbody>
-                                                                    @endif
                                                                 @endif
                                                             @endforeach
                                                         @endforeach
@@ -352,58 +349,6 @@
             </div>
         </div>
     @endforeach
-    <section class="variabel-relevansi">
-        <h5>Relevansi Variabel</h5>
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th scope="col">No</th>
-                    <th scope="col">Deskripsi</th>
-                    <th scope="col">Relevansi</th>
-                    <th scope="col">Ekspektasi</th>
-                    <th scope="col">Realita</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($listVariabel as $lv)
-                    <tr>
-                        <th scope="row">{{ $lv->id }}</th>
-                        <td>{{ $lv->isiVariabel }}</td>
-                        <td>
-                            <form>
-                                <!-- create a list relevansi -->
-                                <select id="myAns" onchange="verifyAnswer()" class="form-select">
-                                    <option value="choose"> --choose -- </option>
-                                    <option value="yes"> Yes </option>
-                                    <option value="no"> No </option>
-                                </select>
-                            </form>
-                        </td>
-                        <td>
-                            <form id="$lv->id">
-                                <label> <input type="radio" name="ekspektasi" id="e1" value="1"> 1 </label>
-                                <label> <input type="radio" name="ekspektasi" id="e2" value="2"> 2 </label>
-                                <label> <input type="radio" name="ekspektasi" id="e3" value="3"> 3 </label>
-                                <label> <input type="radio" name="ekspektasi" id="e4" value="4"> 4 </label>
-                                <label> <input type="radio" name="ekspektasi" id="e5" value="5"> 5 </label>
-
-                            </form>
-                        </td>
-                        <td>
-                            <form id="$lv->id">
-                                <label> <input type="radio" name="realita" id="r1" value="1"> 1 </label>
-                                <label> <input type="radio" name="realita" id="r2" value="2"> 2 </label>
-                                <label> <input type="radio" name="realita" id="r3" value="3"> 3 </label>
-                                <label> <input type="radio" name="realita" id="r4" value="4"> 4 </label>
-                                <label> <input type="radio" name="realita" id="r5" value="5"> 5 </label>
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-
-    </section>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 @endsection
--- monitor-bangunan/resources/views/isisurvey.blade.php
+++ monitor-bangunan/resources/views/isisurvey.blade.php
@@ -3,165 +3,111 @@
 @section('header')
 @include('template.header-landing')
 @endsection
+@section('js')
+<script>
+    function verifyAnswer(noVariabel) {
+        //get the selected value from the dropdown list
+        var x = "myAns"+noVariabel;
+        var mylist = document.getElementById(x);
+        var result = mylist.options[mylist.selectedIndex].text;
+        console.log(result);
+        if (result == 'Tidak Relevan') {
+            //disable all the radio button
+            document.getElementById('e1'+noVariabel).disabled = true;
+            document.getElementById('e2'+noVariabel).disabled = true;
+            document.getElementById('e3'+noVariabel).disabled = true;
+            document.getElementById('e4'+noVariabel).disabled = true;
+            document.getElementById('e5'+noVariabel).disabled = true;
+            document.getElementById('r1'+noVariabel).disabled = true;
+            document.getElementById('r2'+noVariabel).disabled = true;
+            document.getElementById('r3'+noVariabel).disabled = true;
+            document.getElementById('r4'+noVariabel).disabled = true;
+            document.getElementById('r5'+noVariabel).disabled = true;
+        } else {
+            //enable all the radio button
+            document.getElementById('e1'+noVariabel).disabled = false;
+            document.getElementById('e2'+noVariabel).disabled = false;
+            document.getElementById('e3'+noVariabel).disabled = false;
+            document.getElementById('e4'+noVariabel).disabled = false;
+            document.getElementById('e5'+noVariabel).disabled = false;
+            document.getElementById('r1'+noVariabel).disabled = false;
+            document.getElementById('r2'+noVariabel).disabled = false;
+            document.getElementById('r3'+noVariabel).disabled = false;
+            document.getElementById('r4'+noVariabel).disabled = false;
+            document.getElementById('r5'+noVariabel).disabled = false;
+        }
+    }
+</script>
+@endsection
 @section('content')
 <style>
     td{
         width:400px
     }
     label {
-  float: left;
-  padding: 0 1em;
-  text-align: center;
-}
+        float: left;
+        padding: 0 1em;
+        text-align: center;
+    }
 </style>
-    <div class="container-fluid mt-5">
-        <div class="text-center wow fadeInUp mt-5" data-wow-delay="0.1s">
-            <h1 class="mb-5">Silakan Isi Survey di Bawah Ini</h1>
-        </div>
-        <table class="table table-striped">
+<div class="container-fluid mt-5">
+    <div class="text-center wow fadeInUp mt-5" data-wow-delay="0.1s">
+        <h1 class="mb-5">Silakan Isi Survey di Bawah Ini</h1>
+    </div>
+    <form method="POST" action="{{route('simpanHasilSurvey', ['surveyId'=>$surveyId])}}">
+        <table class="table table-bordered">
             <thead>
-              <tr>
-                <th scope="col">No</th>
-                <th scope="col">Kriteria</th>
-                <th scope="col">Ekspektasi</th>
-                <th scope="col">Realita</th>
-              </tr>
+                <tr>
+                    <th scope="col">No</th>
+                    <th scope="col">Deskripsi</th>
+                    <th scope="col">Relevansi</th>
+                    <th scope="col">Ekspektasi</th>
+                    <th scope="col">Realita</th>
+                </tr>
             </thead>
             <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>Bangunan memberikan rasa aman secara fisik (Tidak terjadi kekerasan, Perbuatan Asusila, Pencurian, dll).</td>
-                <td>
-                    <label for="myChoice1">1<br />
-                        <input type="radio" id="myChoice1" name="ekspektasi" value="1" />
-                      </label>
-                      
-                      <label for="myChoice2">2<br />
-                        <input type="radio" id="myChoice2" name="ekspektasi" value="2" />
-                      </label>
-                      
-                      <label for="myChoice3">3<br />
-                        <input type="radio" id="myChoice3" name="ekspektasi" value="3" />
-                      </label>
-                      
-                      <label for="myChoice4">4<br />
-                        <input type="radio" id="myChoice4" name="ekspektasi" value="4" />
-                      </label>
-                      <label for="myChoice4">5<br />
-                        <input type="radio" id="myChoice4" name="ekspektasi" value="5" />
-                      </label>
-                </td>
-                <td>
-                  <label for="myChoice1">1<br />
-                  <input type="radio" id="myChoice1" name="realita" value="1" />
-                </label>
-                
-                <label for="myChoice2">2<br />
-                  <input type="radio" id="myChoice2" name="realita" value="ABC" />
-                </label>
-                
-                <label for="myChoice3">3<br />
-                  <input type="radio" id="myChoice3" name="realita" value="qwerty" />
-                </label>
-                
-                <label for="myChoice4">4<br />
-                  <input type="radio" id="myChoice4" name="realita" value="final" />
-                </label>
-                <label for="myChoice4">5<br />
-                  <input type="radio" id="myChoice4" name="realita" value="final" />
-                </label></td>
-              </tr>
-              <tr>
-                <th scope="row">2</th>
-                <td>Kemudahan berkomunikasi dan pertukaran informasi terhadap pemangku kepentingan.</td>
-                <td>
-                    <label for="myChoice1">1<br />
-                        <input type="radio" id="myChoice1" name="ekspektasi" value="1" />
-                      </label>
-                      
-                      <label for="myChoice2">2<br />
-                        <input type="radio" id="myChoice2" name="ekspektasi" value="ABC" />
-                      </label>
-                      
-                      <label for="myChoice3">3<br />
-                        <input type="radio" id="myChoice3" name="ekspektasi" value="qwerty" />
-                      </label>
-                      
-                      <label for="myChoice4">4<br />
-                        <input type="radio" id="myChoice4" name="ekspektasi" value="final" />
-                      </label>
-                      <label for="myChoice4">5<br />
-                        <input type="radio" id="myChoice4" name="ekspektasi" value="final" />
-                      </label>
-                </td>
-                <td>
-                  <label for="myChoice1">1<br />
-                    <input type="radio" id="myChoice1" name="realita" value="1" />
-                  </label>
-                  
-                  <label for="myChoice2">2<br />
-                    <input type="radio" id="myChoice2" name="realita" value="ABC" />
-                  </label>
-                  
-                  <label for="myChoice3">3<br />
-                    <input type="radio" id="myChoice3" name="realita" value="qwerty" />
-                  </label>
-                  
-                  <label for="myChoice4">4<br />
-                    <input type="radio" id="myChoice4" name="realita" value="final" />
-                  </label>
-                  <label for="myChoice4">5<br />
-                    <input type="radio" id="myChoice4" name="realita" value="final" />
-                  </label>
-                </td>
-              </tr>
-              <tr>
-                <th scope="row">3</th>
-                <td>Masyarakat sekitar tidak merasa terganggu dengan adanya gedung.</td>
-                <td>
-                    <label for="myChoice1">1<br />
-                        <input type="radio" id="myChoice1" name="ekspektasi" value="1" />
-                      </label>
-                      
-                      <label for="myChoice2">2<br />
-                        <input type="radio" id="myChoice2" name="ekspektasi" value="ABC" />
-                      </label>
-                      
-                      <label for="myChoice3">3<br />
-                        <input type="radio" id="myChoice3" name="ekspektasi" value="qwerty" />
-                      </label>
-                      
-                      <label for="myChoice4">4<br />
-                        <input type="radio" id="myChoice4" name="ekspektasi" value="final" />
-                      </label>
-                      <label for="myChoice4">5<br />
-                        <input type="radio" id="myChoice4" name="ekspektasi" value="final" />
-                      </label>
-                </td>
-                <td>
-                  <label for="myChoice1">1<br />
-                    <input type="radio" id="myChoice1" name="realita" value="1" />
-                  </label>
-                  
-                  <label for="myChoice2">2<br />
-                    <input type="radio" id="myChoice2" name="realita" value="ABC" />
-                  </label>
-                  
-                  <label for="myChoice3">3<br />
-                    <input type="radio" id="myChoice3" name="realita" value="qwerty" />
-                  </label>
-                  
-                  <label for="myChoice4">4<br />
-                    <input type="radio" id="myChoice4" name="realita" value="final" />
-                  </label>
-                  <label for="myChoice4">5<br />
-                    <input type="radio" id="myChoice4" name="realita" value="final" />
-                  </label>
-                </td>
-              </tr>
+                @php
+                $i = 1;
+                @endphp
+                @foreach ($listVariabel as $lv)
+
+                <tr>
+                    <th scope="row">{{ $lv->id }}</th>
+                    <td>{{ $lv->isiVariabel }}</td>
+                    <td>
+
+                        @csrf
+                        <!-- create a list relevansi -->
+                        <select id="myAns{{$i}}" onchange="verifyAnswer({{$i}})" class="form-select">
+                            <option value="yes"> Relevan </option>
+                            <option value="no"> Tidak Relevan </option>
+                        </select>
+
+                    </td>
+                    <td>
+                        <label> <input type="radio" name="ekspektasi{{$i}}" id="e1{{$i}}" value="1" class="radioinput{{$i}}"> 1 </label>
+                        <label> <input type="radio" name="ekspektasi{{$i}}" id="e2{{$i}}" value="2" class="radioinput{{$i}}"> 2 </label>
+                        <label> <input type="radio" name="ekspektasi{{$i}}" id="e3{{$i}}" value="3" class="radioinput{{$i}}"> 3 </label>
+                        <label> <input type="radio" name="ekspektasi{{$i}}" id="e4{{$i}}" value="4" class="radioinput{{$i}}"> 4 </label>
+                        <label> <input type="radio" name="ekspektasi{{$i}}" id="e5{{$i}}" value="5" class="radioinput{{$i}}"> 5 </label>
+                    </td>
+                    <td>
+                        <label> <input type="radio" name="realita{{$i}}" id="r1{{$i}}" value="1" class="radioinput{{$i}}"> 1 </label>
+                        <label> <input type="radio" name="realita{{$i}}" id="r2{{$i}}" value="2" class="radioinput{{$i}}"> 2 </label>
+                        <label> <input type="radio" name="realita{{$i}}" id="r3{{$i}}" value="3" class="radioinput{{$i}}"> 3 </label>
+                        <label> <input type="radio" name="realita{{$i}}" id="r4{{$i}}" value="4" class="radioinput{{$i}}"> 4 </label>
+                        <label> <input type="radio" name="realita{{$i}}" id="r5{{$i}}" value="5" class="radioinput{{$i}}"> 5 </label>
+                    </td>
+                </tr>
+                @php
+                $i++;
+                @endphp
+                @endforeach
             </tbody>
-          </table>
-    </div>
+        </table>
+        <button type="btn btn-primary">Submit</button>
+    </form>
+</div>
 @endsection
 @section('footer')
 @include('template.footer-landing')
